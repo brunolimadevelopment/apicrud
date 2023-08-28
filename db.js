9695d695a3723f4df3b8abb0251ea16b44790f3e
Bruno Lima <brunolimadevelopment@gmail.com>
2023-08-28 00:39:42 -0300

    deploy in vercel API

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ import mysql from "mysql"
 
 
 export const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "crud",
+    host: "bfgnmtz7cxf6xm6xaqtu-mysql.services.clever-cloud.com",
+    user: "uyzs5vmi2sb0meyx",
+    password: "B0qKtPpjYFZH0Kpbeq7h",
+    database: "bfgnmtz7cxf6xm6xaqtu",
 });
