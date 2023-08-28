46fb08c8fb05eef86b44c78940b0d86bad06f78a
Bruno Lima <brunolimadevelopment@gmail.com>
2023-08-28 00:28:52 -0300

    deploy in vercel API

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express"
+import userRoutes from "./routes/users.js";
+import cors from "cors"
+
+const app = express() 
+
+app.use(express.json()) // utilizar o modo json para post e put
+app.use(cors())         // evita alguns conflitos rodando localmente
+
+app.use("/", userRoutes); // define a rota userRoutes
+app.listen(8800)        // habilita a porta
